8f50ec18ba8195684bfe5aef30c6cbda4d31faa9
Eric Wuehler <eric.wuehler@mcafee.com>
2018-12-17 12:02:05 -0800

    updated with front/rear and flip rear video

--- convert2mp4.sh
+++ convert2mp4.sh
@@ -12,21 +12,35 @@ else
     exit
 fi
 
-FRONT="/tmp/temp_front.mp4"
-REAR="/tmp/temp_rear.mp4"
-OUTPUT=$(basename ${FILENAME%.*}).mp4
+TEMP_FRONT="/tmp/temp_front.mp4"
+TEMP_REAR="/tmp/temp_rear.mp4"
+FRONT_OUTPUT=$(basename ${FILENAME%.*})_FRONT.mp4
+REAR_OUTPUT=$(basename ${FILENAME%.*})_REAR.mp4
 
-echo "Input file: $(basename ${FILENAME})"
-echo "Output File: $(basename ${OUTPUT})"
+echo "Input file: ${FILENAME}"
+echo "Front Output File: ${FRONT_OUTPUT}"
+echo "Rear Output File: ${REAR_OUTPUT}"
 
+echo "---------------------------------------------"
+echo "Create Temporary files for both cameras"
+echo "---------------------------------------------"
+ffmpeg -i ${FILENAME} -map 0:0 ${TEMP_FRONT} \
+-filter:v "crop=1920:1000:0:0" -map 0:1 -vf hflip ${TEMP_REAR} 
 
-ffmpeg -i ${FILENAME} -map 0:0 ${FRONT} \
--filter:v "crop=1920:900:0:0" -map 0:1 ${REAR} 
+echo "---------------------------------------------"
+echo "Create Front View as Primary"
+echo "---------------------------------------------"
+ffmpeg -y -i ${TEMP_FRONT} -i ${TEMP_REAR} \
+-filter_complex "[1:v] scale=480:250 [1v]; [0:v][1v] overlay=x=1440:y=0 [outv]" \
+-map "[outv]" ${FRONT_OUTPUT}
 
-ffmpeg -y -i ${FRONT} -i ${REAR} \
--filter_complex "[1:v] scale=480:225 [1v]; [0:v][1v] overlay=x=1440:y=0 [outv]" \
--map "[outv]" ${OUTPUT}
+echo "---------------------------------------------"
+echo "Create Rear View as Primary"
+echo "---------------------------------------------"
+ffmpeg -y -i ${TEMP_REAR} -i ${TEMP_FRONT} \
+-filter_complex "[1:v] scale=480:270 [1v]; [0:v][1v] overlay=x=1440:y=0 [outv]" \
+-map "[outv]" ${REAR_OUTPUT}
 
-rm -f ${FRONT}
-rm -f ${REAR}
+rm -f ${TEMP_FRONT}
+rm -f ${TEMP_REAR}
 
